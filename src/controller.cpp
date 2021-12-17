3e06e85a03205b4dd942f191c1a082ec35ac16ad
SamueleD98 <depalo.samuele@gmail.com>
2021-12-17 19:27:21 +0100

    Minor changes

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,45 +1,38 @@
 #include "ros/ros.h"
 #include <iostream>
-#include <math.h>   
+#include <math.h>   	// std::abs
 #include <algorithm>    // std::min
 
 #include "geometry_msgs/Twist.h" 
 #include "sensor_msgs/LaserScan.h"
 #include "rt_assignment2/Command.h"
 
-
-//Variables Declaration 
 ros::Publisher pub;
-ros::Subscriber sub;
+
 geometry_msgs::Twist my_vel;
-float	vel_linear_x = 0.0;
-bool 	helper_status = false;
-//Dividing the ostacles in sectors
-int number_of_sectors = 13;
 
+float target_vel_linear_x = 0.0;//Desired linear velocity value
+float vel_linear_x = 0.0;	//Actual linear velocity value
+bool helper_status = false;	//Helper status (turned on/off)
+int number_of_sectors = 13;	//Number of subsections of the ranges vector
 
-//Functions declaration (the definition after the main)
-
+//Functions declarations (definitions after the main)
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
-
-//Function to discretize the Field Of View. More info in the ReadMe.
-void discretize_fov(const sensor_msgs::LaserScan::ConstPtr& msg, float * m, int number_of_sectors);	
-
-void take_action(float * m);
-
-bool commandCallback(rt_assignment2::Command::Request &req, rt_assignment2::Command::Response &res);
+void discretize_fov(const sensor_msgs::LaserScan::ConstPtr& msg, float * m, int number_of_sectors);	//Function to discretize the Field Of View. More info in the ReadMe.
+void take_action(float * m);										//Function that change angular e linear* velocity. *(Only if the "Helper" is active) 
+bool commandService(rt_assignment2::Command::Request &req, rt_assignment2::Command::Response &res);	//Function that execute the user command given by the service client: the user_interface node
+float angular(float difference, int factor);								//Function that computes the angular velocity as proportional to a given factor
 
 int main (int argc, char **argv)
 {
 	ros::init(argc, argv, "bot_controller");  
 	ros::NodeHandle nh;
 	
-	pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);				//To change the velocity
-	sub = nh.subscribe("/base_scan", 1, laserCallback);		//To see the obstacles
+	pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);				//To change the velocity
 	
-	ros::ServiceServer command = nh.advertiseService("/command", commandCallback);	//To know when to change speed
+	ros::Subscriber sub = nh.subscribe("/base_scan", 1, laserCallback);		//To see the obstacles
 	
-	//ROS_INFO("Executing...\n");
+	ros::ServiceServer command = nh.advertiseService("/command", commandService);	//To receive commands from user_interface node
 	
 	ros::spin();
 	return 0;
@@ -47,44 +40,84 @@ int main (int argc, char **argv)
 
 void take_action(float * m)
 	{
-		float diff = *(m+1) - *(m+3);
-		
-		
-		if (-0.3 < diff < 0.3) 
+		float diff = *(m+1) - *(m+3);	//The difference between opposite subsections
+		if (-0.3 < diff < 0.3) 		//If those are similar
 		{	
-			diff = *(m+0) - *(m+4);
-			if (-0.3 < diff < 0.3)
+			diff = *(m+0) - *(m+4);	//Take the external subsections
+			if (-0.3 < diff < 0.3)	//If even those are similar
 			{
-				my_vel.linear.x = 0.1;
-				my_vel.angular.z = 100;		
+				my_vel.linear.x = 0.1;	//Reduce a lot the speed
+				vel_linear_x = my_vel.linear.x;	//Update the actual speed
+				my_vel.angular.z = 100;	//and turn
 				pub.publish(my_vel);	
 				return;
 			}
 		}
 		
-		my_vel.linear.x = vel_linear_x;	
+		my_vel.linear.x = target_vel_linear_x;	//Take the target speed, if the helper is not active the speed won't change
 		
-		if (*(m+2) < 1.5)
-		{
-			if (helper_status && vel_linear_x >= 2.0) my_vel.linear.x = 0.5;
-			my_vel.angular.z = - diff * 100 / std::abs(diff);
+		int turning_factor;
 		
-		}else if (*(m+2) < 2.0)
+		if (*(m+2) < 1.5) //Very very close obstacle
 		{
-			if (helper_status && vel_linear_x >= 2.0) my_vel.linear.x = 1.0;
-			my_vel.angular.z = - diff * 75 / std::abs(diff);
+			if (helper_status && my_vel.linear.x >= 2.0) my_vel.linear.x = 0.5;	//Reduce the speed if the "Helper" is active			
+			turning_factor = 100;
 		
-		}else if (*(m+2) < 2.3) 
+		}else if (*(m+2) < 2.0)	//Very close obstacle
 		{
-			if (helper_status && vel_linear_x >= 2.0) my_vel.linear.x = 1.5;
-			my_vel.angular.z = - diff * 50 / std::abs(diff);
-		}else{
-			my_vel.angular.z = - diff / std::abs(diff);
+			if (helper_status && my_vel.linear.x >= 2.0) my_vel.linear.x = 1.0;
+			turning_factor = 75;
+					
+		}else if (*(m+2) < 2.5) //Close obstacle
+		{
+			if (helper_status && my_vel.linear.x >= 2.0) my_vel.linear.x = 1.5;
+			turning_factor = 50;
+
+		}else if ((vel_linear_x + 1.0) < target_vel_linear_x )				//If the actual speed is far from the targeted one then the bot has just avoided an obstacle
+		{
+			if (helper_status) my_vel.linear.x = vel_linear_x + 1;			//If the helper is active slowly increase the speed
+			turning_factor = 50;							//Keep turning 
+		}else{	
+			//No obstacles in front of the bot
+			turning_factor = 1;	//Adjust very slowly the robot direction
 		} 	
-				
+		
+		my_vel.angular.z = angular(diff, turning_factor);	//Turn in the best direction
+		
+		vel_linear_x = my_vel.linear.x;	
 		pub.publish(my_vel);	
 	}
 
+bool commandService(rt_assignment2::Command::Request &req, rt_assignment2::Command::Response &res)
+	{
+		switch (req.command)  
+		{		
+			case 1:	//Decreasing speed
+				target_vel_linear_x = target_vel_linear_x - 0.5;
+				break;
+			case 2:	//Increasing speed
+				target_vel_linear_x = target_vel_linear_x + 0.5;	
+				break;		
+			case 3:	//Toggling the helper
+				helper_status = !helper_status;
+				break;
+		}
+		
+		//Filling the response msg
+		res.helper_status = helper_status;	//Set the helper status, if not modified it will be the same
+		res.new_vel = target_vel_linear_x;		//Set the speed, 	 if not modified it will be the same
+		
+		//Publishing the new velocity only if the helper is not active.
+		//If it is active, the take_action function will modify the linear velocity according to the desired one only when possible.
+		if(!helper_status)
+		{
+			my_vel.linear.x = target_vel_linear_x;
+			vel_linear_x = my_vel.linear.x;
+			pub.publish(my_vel);
+		}		
+		return true;
+	}  
+	
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 	{		
 		float min[5] = {10, 10, 10, 10, 10};		
@@ -94,34 +127,6 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 		
 		take_action(m);		
         }
-
-bool commandCallback(rt_assignment2::Command::Request &req, rt_assignment2::Command::Response &res)
-	{
-		switch (req.command)  
-		{		
-			case 1:	//Decreasing speed
-				vel_linear_x = vel_linear_x - 0.5;
-				break;
-			case 2:	//Increasing speed
-				vel_linear_x = vel_linear_x + 0.5;	
-				break;		
-			case 3:	//Toggling the helper
-				helper_status = !helper_status;
-				break;
-		}
-		
-		//filling the response msg
-		res.helper_status = helper_status;
-		res.new_vel = vel_linear_x;
-		
-		//publishing the new velocity
-		my_vel.linear.x = vel_linear_x;
-		pub.publish(my_vel);
-		
-		return true;
-	}  
-
-
 
 void discretize_fov(const sensor_msgs::LaserScan::ConstPtr& msg, float * m, int number_of_sectors)
 	{
@@ -169,28 +174,8 @@ void discretize_fov(const sensor_msgs::LaserScan::ConstPtr& msg, float * m, int 
 		}
 			
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
+float angular(float difference, int factor)
+	{
+		return ( - difference * factor / std::abs(difference) );
+	}
